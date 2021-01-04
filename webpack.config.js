acc3e2fabf4e36f91749c683990417af067fab6a
Stefan Schotvanger <s.schotvanger@gmail.com>
2021-01-04 22:34:39 +0100

    first style.css creation

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,19 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 // JS Directory path.
-const JS_DIR = path.resolve( __dirname, 'assets/javascript/src' );
-const BUILD_DIR = path.resolve(__dirname, 'assets/javascript/dist');
+const JS_DIR = path.resolve( __dirname, 'assets/src/js' );
+const STYLE_DIR = path.resolve( __dirname, 'assets/src/scss' );
+const BUILD_DIR = path.resolve(__dirname, 'assets/dist');
 
 const entry = {
     header: JS_DIR + '/header.js',
     footer: JS_DIR + '/footer.js',
+    style: STYLE_DIR + '/style.scss'
 };
 const output = {
     path: BUILD_DIR,
-    filename: '[name].script.js'
+    filename: 'js/[name].script.js',
 };
 
 module.exports = {
@@ -21,5 +24,25 @@ module.exports = {
     externals: {
         jquery: 'jQuery'
     },
-
+    plugins: [
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: 'css/[name].css',
+            //chunkFilename: '[id].css',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
 };
