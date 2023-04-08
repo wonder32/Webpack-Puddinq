e77af43a98ba12bcdbfe3cd6700055640a0092fa
Stefan Schotvanger <s.schotvanger@gmail.com>
2023-04-08 17:30:17 +0200

    fix search sign

--- searchform.php
+++ searchform.php
@@ -4,4 +4,4 @@
     <input type="search" class="search-field" placeholder="<?php _e('Search query', 'puddinq-com'); ?>" value="<?= get_search_query(); ?>" name="s" />
 </label>
 <input type="submit" class="search-submit" value="<?php _e('Search', 'puddinq-com'); ?>" />
-</form>';
+</form>
